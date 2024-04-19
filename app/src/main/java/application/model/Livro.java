26e0849b844750f763e24be1d457c272548b2f28
Jacqueline Akina Nakagawa <127306288+JacqueNakagawa@users.noreply.github.com>
2024-04-19 19:40:59 +0000

    Aula - 19-04

---
+++ app/src/main/java/application/model/Livro.java
@@ -0,0 +1,41 @@
+package application.model;
+
+import java.lang.annotation.Inherited;
+
+import javax.annotation.processing.Generated;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "livros")
+public class Livro {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String titulo;
+    private String genero;
+    public long getId() {
+        return id;
+    }
+    public void setId(long id) {
+        this.id = id;
+    }
+    public String getTitulo() {
+        return titulo;
+    }
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+    public String getGenero() {
+        return genero;
+    }
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+
+    
+}
